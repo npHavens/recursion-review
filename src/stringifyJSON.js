c8588a4afe8c2cc96d4ee8021700f28c8db5fb5e
Nick Havens <npahavens@gmail.com>
2017-05-17 15:18:40 -0500

    Started implementation of stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,9 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function ()  {
+var stringifyJSON = function(obj)  {
+//if the obj is falsey
+if(obj === null){
+  return 'null';
+}
 
-  //some change to commit
+else if (typeof obj === 'boolean'|| typeof obj === 'number'){
+  return obj.toString();
+}
+
+else if (typeof obj === 'string'){
+  return ()
+}
+
+//deals with array cases
+else if(Array.isArray(obj)){
+  var arrayString = '[';
+  for( var i = 0; i < obj.length; i++){
+    if( i > 0){
+      arrayString+= ',';
+    }
+    arrayString += stringifyJSON(obj[i]);
+  }
+  arrayString+= ']';
+  return arrayString;
+}
+
+
+
+
+
+
+
+
+
 
 
 
